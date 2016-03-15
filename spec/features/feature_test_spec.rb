759480d91c2426e93167809de7d38e641110162f
Hanif Fakira <fakira.work@gmail.com>
2016-03-15 14:03:22 +0000

    Q18 done

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -5,14 +5,12 @@ require 'capybara/rspec'
 require 'capybara/dsl'
 Capybara.default_driver = :selenium
 
-RSpec.feature "Battle game", :type => :feature do
-  scenario "Allows users to enter names" do
-    visit "http://localhost:9393/"
-
-    fill_in 'player_1_name', with: 'Bob'
-    fill_in 'player_2_name', with: 'Jim'
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('localhost:9393/')
+    fill_in :player_1_name, with: 'Bob'
+    fill_in :player_2_name, with: 'Jim'
     click_button 'Submit'
-
-    expect(page).to have_text("Bob")
+    expect(page).to have_content 'Bob vs Jim'
   end
 end
