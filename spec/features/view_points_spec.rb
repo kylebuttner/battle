312bfe4cf34e9b3e7230a13f4db2b8f85ff34100
Hanif Fakira <fakira.work@gmail.com>
2016-03-15 14:39:18 +0000

    Q20 Done

---
+++ spec/features/view_points_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+require "./lib/app.rb"
+require 'rspec'
+require 'capybara/rspec'
+require 'capybara/dsl'
+Capybara.default_driver = :selenium
+
+feature 'view hit points' do
+  scenario 'show hit points' do
+    visit('localhost:9393/')
+    fill_in :player_1_name, with: 'Bob'
+    fill_in :player_2_name, with: 'Jim'
+    click_button 'Submit'
+    expect(page).to have_content 'Jim: 100 HP'
+  end
+end
