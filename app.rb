7a881d184b39b8e0961d8a516ba5e4149d6bd9b0
Hanif Fakira <fakira.work@gmail.com>
2016-03-15 14:22:51 +0000

    Q19 Done

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
+enable :sessions
 
 get '/' do
   erb(:index)
 end
 
 post '/names' do
-  @name1 = params[:player_1_name]
-  @name2 = params[:player_2_name]
+  session[:player_1_name] = params[:player_1_name]
+  session[:player_2_name] = params[:player_2_name]
+  redirect '/play'
+end
+
+get '/play' do
+  @name1 = session[:player_1_name]
+  @name2 = session[:player_2_name]
   erb(:play)
 end
