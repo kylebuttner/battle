987b00dbe9e406dd3e3379f8abef92ae49a5a031
Misa Ogura <misa.ogura01@gmail.com>
2016-03-15 19:52:23 +0000

    Complete #20

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,9 +1,6 @@
 RSpec.feature "0. User Entry", :type => :feature do
 
-  # let(:user1) { }
-  # let(:user2) { }
-
-  scenario "0.0. users enter names and page displays names" do
+  scenario "users enter names and page displays names" do
     visit '/'
     fill_in(:player1, with: 'Misa')
     fill_in(:player2, with: 'Pete')
---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,20 @@
+RSpec.feature "1. Display hitpoints", :type => :feature do
+
+  let(:p2_hp) { '60' }
+
+  scenario 'Page displays player2 hitpoints as text' do
+    visit '/'
+    fill_in(:player1, with: 'Misa')
+    fill_in(:player2, with: 'Pete')
+    click_button 'Enter Battle!!'
+    expect(page).to have_content "Player 2 HP: #{p2_hp}"
+  end
+
+#   scenario '1.1 Page displays player2 hitpoints as progress bar' do
+#     visit '/'
+#     fill_in(:player1, with: 'Misa')
+#     fill_in(:player2, with: 'Pete')
+#     click_button 'Enter Battle!!'
+#     expect(page.has_xpath?("//progress[@max=60][@value=60]"))
+#   end
+end
