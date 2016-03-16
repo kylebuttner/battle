0a7e0a2b2cbaf89f71107370f03bf096fc28c683
Kyle Buttner <kylebuttner@gmail.com>
2016-03-16 10:41:50 +0000

    add spec helper

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 
 feature "hit points" do
   scenario "view opponents hit points" do
-    visit "/"
-
-    fill_in "player_1_name", :with => "ass"
-    fill_in "player_2_name", :with => "boob"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content "100 HP"
   end
 end
--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 
 feature "name entry" do
   scenario "users enters new name" do
-    visit "/"
-
-    fill_in "player_1_name", :with => "ass"
-    fill_in "player_2_name", :with => "boob"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content "ass vs. boob"
   end
 end
