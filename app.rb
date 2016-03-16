de7e9c3fc8f5583e3edc5856c43ff198a9cbbc73
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-16 11:48:10 +0000

    Created Game class and spec. Initiating attack using Game class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/game'
 require_relative './lib/player'
 
 class Battle < Sinatra::Base
@@ -12,6 +13,7 @@ class Battle < Sinatra::Base
   post '/names' do
     $player1 = Player.new(params[:player1])
     $player2 = Player.new(params[:player2])
+    $game = Game.new($player1, $player2)
     redirect '/play'
   end
 
@@ -23,9 +25,10 @@ class Battle < Sinatra::Base
   end
 
   get '/attacked' do
+    @game = $game
     @player1_name = $player1.name
     @player2_name = $player2.name
-    @p2_hp = $player2.receive_attack
+    @p2_hp = @game.attack($player2)
     erb(:attacked)
   end
 
---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Game
+
+  def initialize(player1, player2)
+    
+  end
+
+  def attack(player)
+    player.receive_attack
+  end
+end
